759df0ca9586872cf72022f3e6beccf86f79da2a
heitor windows <heitorbrandolin@hotmail.com>
2024-09-03 22:16:31 -0300

    Refatorado os componentes de userIcon e perfil e adicionado a foto de perfil na tela inicial

--- front-end/src/app/(tabs)/perfil.tsx
+++ front-end/src/app/(tabs)/perfil.tsx
@@ -1,6 +1,6 @@
 import UserIcon from '@/src/components/userIcon';
 import Constants from 'expo-constants';
-import { ScrollView, Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, Text, View } from 'react-native';
 import * as data from '@/db.json'
 import { CardConfig } from '@/src/components/cardConfig';
 import NotificationCard from '@/src/components/cardNotification';
@@ -9,22 +9,46 @@ const statusBarHeight = Constants.statusBarHeight;
 
 export default function Perfil() {
 	const user = data.user[0];
+
 	return (
-		<ScrollView
-			style={{ flex: 1 }}
-			className="bg-white"
-			showsVerticalScrollIndicator={false}
-		>
-			<View className="w-full px-4 py-8" style={{ marginTop: statusBarHeight + 8 }}>
-				<UserIcon image={user.image} />
-				<Text
-					className='font-bold text-center text-2xl'
-				>
-					{user.name}
-				</Text>
-			</View>
-			<NotificationCard/>
-			<CardConfig/>
-		</ScrollView>
+		<SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
+			<ScrollView
+				style={{ flex: 1 }}
+				className="bg-white"
+				showsVerticalScrollIndicator={false}
+			>
+				<View className="w-full px-4 py-8" style={{ marginTop: statusBarHeight }}>
+					<UserIcon image={user.image} style="w-24 h-24 rounded-full border-2 border-black" />
+					<Text className="font-bold text-center text-2xl">
+						{user.name}
+					</Text>
+				</View>
+				<NotificationCard />
+				<CardConfig
+					icon="person"
+					title="Minha conta"
+					subtitle="Meus dados"
+					onPress={() => console.log("Clicou em Minha conta")}
+				/>
+				<CardConfig
+					icon="wallet"
+					title="Pagamentos"
+					subtitle="Informações de pagamento"
+					onPress={() => console.log("Clicou em Pagamentos")}
+				/>
+				<CardConfig
+					icon="notifications"
+					title="Notificações"
+					subtitle="Minha central de notificações"
+					onPress={() => console.log("Clicou em Notificações")}
+				/>
+				<CardConfig
+					icon="history"
+					title="Histórico"
+					subtitle="Meu histórico de alocações"
+					onPress={() => console.log("Clicou em Histórico")}
+				/>
+			</ScrollView>
+		</SafeAreaView>
 	);
 }
--- front-end/src/app/_layout.tsx
+++ front-end/src/app/_layout.tsx
@@ -1,11 +1,13 @@
 import { HoverEffect } from "react-native-gesture-handler";
 import "../styles/global.css"
 import { Stack } from 'expo-router';
-import { StatusBar } from "react-native";
+import { SafeAreaView } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
     <>
+      <StatusBar style="dark" translucent={true} backgroundColor="transparent" />
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="(screens)" options={{ headerShown: false }} />
