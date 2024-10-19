40c7e5b1812aa646ede8d7dc187f683146a14c5f
Eduardo Richard <dudurichard07@gmail.com>
2024-10-19 00:57:08 -0300

    Adicionado atributo para imagens, remove e insere arrays de paths do bucket.

--- back-end/src/domains/gestao/estabelecimento/estabelecimento.module.ts
+++ back-end/src/domains/gestao/estabelecimento/estabelecimento.module.ts
@@ -1,18 +1,20 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module }         from '@nestjs/common';
+import { TypeOrmModule }  from '@nestjs/typeorm';
 
-import { Estabelecimento } from './entities/estabelecimento.entity';
-import { EstabelecimentoService } from './estabelecimento.service';
-import { EstabelecimentoController } from './estabelecimento.controller';
+import { Estabelecimento }            from './entities/estabelecimento.entity';
+import { EstabelecimentoService }     from './estabelecimento.service';
+import { EstabelecimentoController }  from './estabelecimento.controller';
 
-import { UsuarioModule } from '@src/domains/auth/usuario/usuario.module';
-import { QuadraModule } from './quadra/quadra.module';
+import { QuadraModule }   from './quadra/quadra.module';
+import { UsuarioModule }  from '@src/domains/auth/usuario/usuario.module';
+import { ImagemModule }   from '@src/domains/storage/imagem/imagem.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Estabelecimento]), 
     QuadraModule,
-    UsuarioModule
+    UsuarioModule,
+    ImagemModule
   ], 
   controllers: [EstabelecimentoController],
   providers: [EstabelecimentoService],
