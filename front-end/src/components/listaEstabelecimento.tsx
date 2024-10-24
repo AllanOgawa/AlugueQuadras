c3e960cff933f54b38fcb351723e6e802b5b97f2
Heitor Brandolin <60556988+devhetor@users.noreply.github.com>
2024-10-24 00:17:14 -0300

    Update listaEstabelecimento.tsx

--- front-end/src/components/listaEstabelecimento.tsx
+++ front-end/src/components/listaEstabelecimento.tsx
@@ -1,208 +1,163 @@
-<<<<<<< HEAD
-import React from 'react';
-import { View, Text, Image, FlatList, StyleSheet } from 'react-native';
-
-interface Estabelecimento {
-    id: string;
-    name: string;
-    endereco: string;
-    avaliacao: number;
-    image: { id: string; name: string; image: string }[];
-}
-
-interface Props {
-    data: Estabelecimento[];
-}
-
-const ListaEstabelecimento: React.FC<Props> = ({ data }) => {
-    // Renderiza cada estabelecimento da lista
-    const renderEstabelecimento = ({ item }: { item: Estabelecimento }) => (
-        <View style={styles.card}>
-            <Text style={styles.name}>{item.name}</Text>
-            <Text style={styles.endereco}>{item.endereco}</Text>
-            <Text style={styles.avaliacao}>Avaliação: {item.avaliacao.toFixed(1)} ⭐</Text>
-            <FlatList
-                data={item.image}
-                horizontal
-                keyExtractor={(image) => image.id}
-                renderItem={({ item: img }) => (
-                    <Image source={{ uri: img.image }} style={styles.image} />
-                )}
-                showsHorizontalScrollIndicator={false}
-            />
-        </View>
-    );
-
-    return (
-        <FlatList
-            data={data}
-            keyExtractor={(item) => item.id}
-            renderItem={renderEstabelecimento}
-            contentContainerStyle={styles.container}
-        />
-=======
-import React, { useEffect, useState } from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity, ActivityIndicator } from 'react-native';
-import { EstabelecimentoProps } from '../interfaces/estabelecimento';
-import { MaterialIcons } from '@expo/vector-icons';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import IconUsuario from '@components/iconUsuario';
 import Constants from 'expo-constants';
+import { SafeAreaView, ScrollView, StatusBar, Text, View } from 'react-native';
+import { CardConfig } from '@components/cardConfig';
+import NotificationCard from '@components/cardNotification';
+import { router } from 'expo-router';
+import { useContext, useEffect, useState } from 'react';
+import { UsuarioContext } from '@context/usuarioContext';
+import Loading from '@/src/components/loading';
 
-const apiUrl = Constants.expoConfig?.extra?.apiUrl || '';
+const statusBarHeight = Constants.statusBarHeight;
+const { bucketUrl, userDefaultImage } = Constants.expoConfig.extra;
 
-interface RenderOptions {
-    showImage?: boolean;
-    showAvaliacao?: boolean;
-    showPreco?: boolean;
-    showAcomodacoes?: boolean;
+export default function Perfil() {
+	const [loading, setLoading] = useState(false);
+	const [nome, setNome] = useState("Usuário não Logado");
+	const [imagem, setImagem] = useState(userDefaultImage);
+
+	const context = useContext(UsuarioContext);
+	if (!context) {
+		throw new Error("YourComponent must be used within an ArrayProvider");
+	}
+	const { usuario, setUsuario } = context;
+
+	useEffect(() => {
+		if (usuario != null && usuario[0] !== null) {
+			if (usuario[0].nome) {
+				setNome(usuario[0].nome);
+			}
+			if (usuario[0].imagens && usuario[0].imagens[0].path) {
+				setImagem(usuario[0].imagens[0].path);
+			}
+		}
+		setLoading(false);
+	}, [usuario]);
+
+	// if (false) {
+	// 	return (
+	// 		<SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
+	// 			<StatusBar barStyle="dark-content" backgroundColor="white" />
+	// 			<ScrollView
+	// 				style={{ flex: 1 }}
+	// 				className="bg-white"
+	// 				showsVerticalScrollIndicator={false}
+	// 			>
+	// 				<View className="w-full px-4 py-8" style={{ marginTop: statusBarHeight }}>
+	// 					<IconUsuario image={user.image} style="w-24 h-24 rounded-full border-2 border-black" />
+	// 					<Text className="font-bold text-center text-2xl mt-2">
+	// 						{user.name}
+	// 					</Text>
+	// 				</View>
+	// 				<NotificationCard />
+	// 				<CardConfig
+	// 					icon="person"
+	// 					title="Minha conta"
+	// 					subtitle="Meus dados"
+	// 					style='h-16 w-full rounded-2xl flex-row items-center justify-between px-4'
+	// 					onPress={() => console.log("Clicou em Minha conta")}
+	// 				/>
+	// 				<CardConfig
+	// 					icon="monetization-on"
+	// 					title="Financeiro"
+	// 					subtitle="Minhas Finanças"
+	// 					style='h-16 w-full rounded-2xl flex-row items-center justify-between px-4'
+	// 					onPress={() => console.log("Clicou em Financas")}
+	// 				/>
+	// 				<CardConfig
+	// 					icon="notifications"
+	// 					title="Notificações"
+	// 					subtitle="Minha central de notificações"
+	// 					style='h-16 w-full rounded-2xl flex-row items-center justify-between px-4'
+	// 					onPress={() => router.navigate('/(screens)/notification')}
+	// 				/>
+	// 				<CardConfig
+	// 					icon="sports-tennis"
+	// 					title="Tenho um negócio"
+	// 					subtitle="Torne-se um parceiro nosso!"
+	// 					style="h-16 w-full rounded-2xl flex-row items-center justify-between px-4"
+	// 					onPress={() => {
+	// 						if (user.adm) {
+	// 							router.push('/(estabelecimento)/home')
+	// 						} else {
+	// 							console.log("Usuário não é administrador, ação não permitida.");
+	// 						}
+	// 					}}
+	// 				/>
+
+	// 				<CardConfig
+	// 					icon="sell"
+	// 					title="Promoções"
+	// 					subtitle="Minhas promoções"
+	// 					style='h-16 w-full rounded-2xl flex-row items-center justify-between px-4'
+	// 					onPress={() => console.log("Clicou em Promoções")}
+	// 				/>
+
+	// 			</ScrollView>
+	// 			{loading && <Loading />}
+	// 		</SafeAreaView>
+	// 	);
+	// }
+
+	return (
+		<SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
+			<StatusBar barStyle="dark-content" backgroundColor="white" />
+			<ScrollView
+				style={{ flex: 1 }}
+				className="bg-white"
+				showsVerticalScrollIndicator={false}
+			>
+				<View className="w-full px-4 py-8" style={{ marginTop: statusBarHeight }}>
+					<IconUsuario image={`${bucketUrl}/${imagem}`} style="w-24 h-24 rounded-full border-2 border-black" />
+					<Text className="font-bold text-center text-2xl">
+						{nome}
+					</Text>
+				</View>
+				<NotificationCard />
+				<CardConfig
+					icon="person"
+					title="Minha conta"
+					subtitle="Meus dados"
+					style='h-16 w-full rounded-2xl flex-row items-center justify-between px-4'
+					onPress={() => router.push('/(usuario)/editar')}
+				/>
+				<CardConfig
+					icon="wallet"
+					title="Pagamentos"
+					subtitle="Informações de pagamento"
+					style='h-16 w-full rounded-2xl flex-row items-center justify-between px-4'
+					onPress={() => console.log("Clicou em Pagamentos")}
+				/>
+				<CardConfig
+					icon="notifications"
+					title="Notificações"
+					subtitle="Minha central de notificações"
+					style='h-16 w-full rounded-2xl flex-row items-center justify-between px-4'
+					onPress={() => router.navigate('/notification')}
+				/>
+				<CardConfig
+					icon="history"
+					title="Histórico"
+					subtitle="Meu histórico de alocações"
+					style='h-16 w-full rounded-2xl flex-row items-center justify-between px-4'
+					onPress={() => console.log("Clicou em Histórico")}
+				/>
+				<CardConfig
+					icon="sports-tennis"
+					title="Quadras"
+					subtitle="Minhas quadras"
+					style='h-16 w-full rounded-2xl flex-row items-center justify-between px-4'
+					onPress={() => router.push('/(quadra)/menu')}
+				/>
+				<CardConfig
+					icon="sports-tennis"
+					title="Estabelecimento"
+					subtitle="Minhas quadras"
+					style='h-16 w-full rounded-2xl flex-row items-center justify-between px-4'
+					onPress={() => router.push('/(estabelecimento)/menu')}
+				/>
+			</ScrollView>
+			{loading && <Loading />}
+		</SafeAreaView>
+	);
 }
-
-interface Props {
-    onPress: (estabelecimento: EstabelecimentoProps) => void;
-    options?: RenderOptions;
-}
-
-const ListaEstabelecimento: React.FC<Props> = ({ onPress, options = {} }) => {
-    const [data, setData] = useState<EstabelecimentoProps[]>([]);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [error, setError] = useState<string | null>(null);
-
-    useEffect(() => {
-        const fetchEstabelecimentos = async () => {
-            try {
-                const access_token = await AsyncStorage.getItem('access_token');
-                const response = await fetch(`${apiUrl}/estabelecimento/usuario`, {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${access_token}`,
-                    },
-                });
-
-                if (!response.ok) {
-                    throw new Error('Erro ao buscar os dados.');
-                }
-
-                const jsonData = await response.json();
-                setData(jsonData);
-                setLoading(false);
-            } catch (err) {
-                console.log(err);
-                setError('Erro ao carregar os dados.');
-                setLoading(false);
-            }
-        };
-
-        fetchEstabelecimentos();
-    }, []);
-
-    const renderEstabelecimento = (item: EstabelecimentoProps) => (
-        <TouchableOpacity style={styles.card} onPress={() => onPress(item)} key={item.idkey}>
-            {options.showImage && item.imagens && item.imagens.length > 0 && (
-                <Image source={{ uri: `${apiUrl}/${item.imagens[0].path}` }} style={styles.image} />
-            )}
-            <View style={styles.infoContainer}>
-                <Text style={styles.name}>{item.nome}</Text>
-                <Text style={styles.endereco}>
-                    {item.endereco.logradouro}, {item.endereco.numero} - {item.endereco.bairro}, {item.endereco.cidade} - {item.endereco.estado}
-                </Text>
-                {options.showAvaliacao && (
-                    <Text style={styles.avaliacao}>Avaliação: N/A</Text>
-                )}
-                {options.showPreco && (
-                    <Text style={styles.preco}>Preço: N/A</Text>
-                )}
-            </View>
-        </TouchableOpacity>
-    );
-
-    if (loading) {
-        return <ActivityIndicator size="large" color="#FF6600" />;
-    }
-
-    if (error) {
-        return <Text>{error}</Text>;
-    }
-
-    return (
-        <View>
-            {data.map(item => renderEstabelecimento(item))}
-        </View>
->>>>>>> 2cef1aa93ebd66ae32746187fd6ec38bf08de022
-    );
-};
-
-const styles = StyleSheet.create({
-<<<<<<< HEAD
-    container: {
-        paddingHorizontal: 16,
-        paddingVertical: 16,
-    },
-    card: {
-        backgroundColor: '#fff',
-        borderRadius: 8,
-        padding: 16,
-        marginBottom: 16,
-=======
-    card: {
-        backgroundColor: '#fff',
-        borderRadius: 8,
-        flexDirection: 'row',
-        padding: 16,
-        marginBottom: 16,
-        marginHorizontal: 8,
->>>>>>> 2cef1aa93ebd66ae32746187fd6ec38bf08de022
-        shadowColor: '#000',
-        shadowOpacity: 0.1,
-        shadowRadius: 8,
-        elevation: 3,
-    },
-<<<<<<< HEAD
-    name: {
-        fontSize: 18,
-        fontWeight: 'bold',
-=======
-    image: {
-        width: 150,
-        height: 150,
-        borderRadius: 10,
-        marginRight: 16,
-    },
-    infoContainer: {
-        flex: 1,
-        justifyContent: 'center',
-    },
-    name: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        color: '#FF6600',
->>>>>>> 2cef1aa93ebd66ae32746187fd6ec38bf08de022
-        marginBottom: 8,
-    },
-    endereco: {
-        fontSize: 14,
-        color: '#666',
-<<<<<<< HEAD
-        marginBottom: 8,
-=======
-        marginBottom: 4,
->>>>>>> 2cef1aa93ebd66ae32746187fd6ec38bf08de022
-    },
-    avaliacao: {
-        fontSize: 14,
-        marginBottom: 12,
-    },
-<<<<<<< HEAD
-    image: {
-        width: 100,
-        height: 100,
-        borderRadius: 8,
-        marginRight: 8,
-=======
-    preco: {
-        fontWeight: 'bold',
->>>>>>> 2cef1aa93ebd66ae32746187fd6ec38bf08de022
-    },
-});
-
-export default ListaEstabelecimento;
