28cc64b45aa6b531e2c491febcc8eaf4411b3058
Karla Duarte Ferreira <karladuarte87@hotmail.com>
2024-11-14 22:56:50 -0300

    feature: route development with available times

--- back-end/src/domains/gestao/estabelecimento/quadra/reserva/reserva.service.ts
+++ back-end/src/domains/gestao/estabelecimento/quadra/reserva/reserva.service.ts
@@ -8,20 +8,20 @@ import {
   Inject,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository, LessThan, MoreThan, Not } from 'typeorm';
+import { Repository, LessThan, MoreThan, Not, Between } from 'typeorm';
 import { Reserva } from './entities/reserva.entity';
 import { CreateReservaDto } from './dto/create-reserva.dto';
 import { QuadraService } from '../quadra.service';
-import { exec } from 'child_process';
+import { DiaSemana } from '../../horario-funcionamento/enums/dia-semana.enum';
 
 @Injectable()
 export class ReservaService {
   constructor(
-    @InjectRepository(Reserva)
-    private readonly reservaRepository: Repository<Reserva>,
-
     @Inject(forwardRef(() => QuadraService))
     private readonly quadraService: QuadraService,
+
+    @InjectRepository(Reserva)
+    private readonly reservaRepository: Repository<Reserva>,
   ) {}
 
   async create(
@@ -112,7 +112,10 @@ export class ReservaService {
       });
 
       if (!reserva) {
-        throw new HttpException('Reserva não encontrada', HttpStatus.NOT_FOUND);
+        throw new HttpException(
+          'Reserva não encontradaa',
+          HttpStatus.NOT_FOUND,
+        );
       }
 
       if (reserva.cancelada) {
@@ -128,6 +131,116 @@ export class ReservaService {
       console.error('Erro ao cancelar reserva:', error.message);
       throw new HttpException(
         'Erro ao cancelar reserva',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async findAvailableSlots(quadraIdkey: number): Promise<any> {
+    try {
+      console.log(`Buscando quadra com idkey: ${quadraIdkey}`);
+      const quadra = await this.quadraService.findByIdkey(quadraIdkey);
+
+      if (!quadra) {
+        console.error('Quadra não encontrada');
+        throw new NotFoundException('Quadra não encontrada');
+      }
+
+      const estabelecimento = quadra.estabelecimento;
+      console.log(`Estabelecimento encontrado: ${estabelecimento.idkey}`);
+
+      const dataAtual = new Date();
+      const horariosDisponiveis = [];
+
+      for (let dia = 0; dia < 20; dia++) {
+        const data = new Date(dataAtual);
+        data.setDate(dataAtual.getDate() + dia);
+
+        const diaSemana = data.getDay();
+        console.log(`Verificando horários para o dia da semana: ${diaSemana}`);
+
+        const horarioFuncionamento = estabelecimento.horariosFuncionamento.find(
+          (horario) => horario.diaSemana === diaSemana,
+        );
+
+        if (!horarioFuncionamento) {
+          console.log(
+            `Nenhum horário de funcionamento encontrado para o dia ${diaSemana}`,
+          );
+          continue;
+        }
+
+        console.log(
+          `Horário de funcionamento encontrado: Abertura - ${horarioFuncionamento.horaAbertura}, Fechamento - ${horarioFuncionamento.horaFechamento}`,
+        );
+
+        const [horaA, minutoA] = horarioFuncionamento.horaAbertura
+          .split(':')
+          .map(Number);
+        const horarioInicio = new Date(data);
+        horarioInicio.setHours(horaA, minutoA, 0, 0);
+
+        const [horaF, minutoF] = horarioFuncionamento.horaFechamento
+          .split(':')
+          .map(Number);
+        const horarioFim = new Date(data);
+        horarioFim.setHours(horaF, minutoF, 0, 0);
+
+        console.log(
+          `Horário de início: ${horarioInicio.toISOString()}, Horário de fim: ${horarioFim.toISOString()}`,
+        );
+
+        const reservasDia = await this.reservaRepository.find({
+          where: {
+            quadra: quadra,
+            dataInicio: Between(
+              new Date(data.setHours(horaA, 0, 0, 0)),
+              new Date(data.setHours(horaF, 59, 59, 999)),
+            ),
+          },
+        });
+
+        console.log(
+          `Reservas para o dia ${data.toISOString().split('T')[0]}:`,
+          reservasDia,
+        );
+
+        const horasDisponiveis = [];
+
+        for (
+          let hora = new Date(horarioInicio);
+          hora < horarioFim;
+          hora.setHours(hora.getHours() + 1)
+        ) {
+          const horaInicioStr = hora.toTimeString().slice(0, 8);
+          const horaFimStr = new Date(hora);
+          horaFimStr.setHours(hora.getHours() + 1);
+
+          if (
+            !reservasDia.some(
+              (reserva) => reserva.dataInicio <= hora && reserva.dataFim > hora,
+            )
+          ) {
+            horasDisponiveis.push({
+              horaInicio: horaInicioStr,
+              horaFim: horaFimStr.toTimeString().slice(0, 8),
+            });
+          }
+        }
+
+        if (horasDisponiveis.length > 0) {
+          horariosDisponiveis.push({
+            data: data.toISOString().split('T')[0],
+            horas: horasDisponiveis,
+          });
+        }
+      }
+
+      return horariosDisponiveis;
+    } catch (error) {
+      console.error('Erro ao buscar horários disponíveis:', error);
+      throw new HttpException(
+        'Erro ao buscar horários disponíveis',
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
